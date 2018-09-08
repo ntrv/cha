cef3e6c035c512136a4518ae2340aefb1a2f346b
ntrv <hydrangea56@me.com>
2018-09-09 06:42:19 +0900

    Add API client for /me

---
+++ api_me.go
@@ -0,0 +1,21 @@
+package cha
+
+import (
+	"context"
+	"encoding/json"
+
+	cw "github.com/griffin-stewie/go-chatwork"
+)
+
+func (c Client) MeContext(ctx context.Context) (me cw.Me, err error) {
+	res, err := c.get(ctx, "/me", map[string]string{})
+	if err != nil {
+		return
+	}
+	err := json.Unmarshal(res, &me)
+	return
+}
+
+func (c Client) Me() (me cw.Me, err error) {
+	return c.MeContext(context.Background())
+}
