a83ca3e085188b992f8da524ab4002fd8c15d143
ntrv <hydrangea56@me.com>
2018-09-09 15:28:27 +0900

    Add /my API

--- api_my.go
+++ api_my.go
@@ -16,6 +16,35 @@ func (c Client) MyStatusContext(ctx context.Context) (status cw.Status, err erro
 	return
 }
 
-func (c Client) MyStatus() (status cw.Status, err error) {
+func (c Client) MyStatus() (cw.Status, error) {
 	return c.MyStatusContext(context.Background())
+}
+
+func (c Client) MyTasksContext(
+	ctx context.Context,
+	params map[string]string,
+) (tasks []cw.MyTask, err error) {
+	res, err := c.get(ctx, "/my/tasks", params)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(res, &tasks)
+	return
+}
+
+func (c Client) MyTasks(params map[string]string) ([]cw.MyTask, error) {
+	return c.MyTasksContext(context.Background(), params)
+}
+
+func (c Client) ContactsContext(ctx context.Context) (contacts []cw.Contact, err error) {
+	res, err := c.get(ctx, "/contacts", map[string]string{})
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(res, &contacts)
+	return
+}
+
+func (c Client) Contacts() ([]cw.Contact, error) {
+	return c.ContactsContext(context.Background())
 }
